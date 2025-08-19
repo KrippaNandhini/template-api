76b033c1559c466d02eaa0385fceac91316de4f4
KrippaNandhini <krippamail@gmail.com>
2025-08-19 21:34:15 +0530

    Final commit

--- tests/test_contracts_smoke.py
+++ tests/test_contracts_smoke.py
@@ -1,6 +1,6 @@
 from app.settings import Settings
 
 
-def test_settings_loads_from_env_defaults() -> None:  # ← add -> None
+def test_settings_loads_from_env_defaults() -> None:
     s = Settings()
     assert s.APP_NAME != ""
